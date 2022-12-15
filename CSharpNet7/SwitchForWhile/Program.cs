d355d1b5a3dff021822474eb0328387aae3f5a8d
phan ha trung tin <phanhatrungtin893@hotmail.com>
2022-12-15 14:46:09 +0700

    finish switchforwhile

---
+++ CSharpNet7/SwitchForWhile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+int number1 = Random.Shared.Next(0, 9);
+int number2 = Random.Shared.Next(0, 9);
+Console.WriteLine("vui long nhap phep toan:");
+Console.WriteLine($"{nameof(number1)}={number1},{nameof(number2)}={number2}");
+string? dau = Console.ReadLine();
+switch (dau)
+{
+    case "+" when number1 > 0 && number2 > 0:
+        Console.WriteLine(number1 + number2);
+        break;
+
+    case "-" when number1>number2:
+        Console.WriteLine(number1 - number2);
+        break;
+    case "*":
+        Console.WriteLine(number1 * number2);
+        break;
+
+    case "/" when number2!=0:
+        Console.WriteLine(number1 / number2);
+        break;
+        default: Console.WriteLine("no catulate"); break;
+}
+//footmart code: ctrl k+d
+
+//switch expression
+Console.WriteLine(
+    dau switch
+    {
+        "+" when number1 > 0 && number2 > 0 => number1 + number2,
+        "-" when number1 > number2 => number1 - number2,
+        "*" => number1 * number2,
+        "/" when number2 != 0 => number1 / number2,
+        _=> "no catulate"
+    }
+    ) ;
+;
